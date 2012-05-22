e45e071631314403a5b43a9c7d98caf2e328c304
Muhammad M Rahman <mmrs151@gmail.com>
2012-05-22 07:34:00 +0100

    Finished whois lookup for uk domains

--- whoislookup.py
+++ whoislookup.py
@@ -21,25 +21,24 @@ class MyParser(HTMLParser):
 
 class WhoisLookup(object):
     def __init__(self, url):
-        self.url = WHOISHOST + url
-        p = MyParser(self.url)
-        self.raw_data = self.clean_data(p.stack)
-        self.domain = self.get_domain(self.raw_data).strip()
-        self.registrant = self.get_registrant(self.raw_data).strip()
-        self.registrant_type = self.get_registrant_type(self.raw_data).strip()
-        """
-        self.registrant_address = 
-        self.registrar = 
-        self.relevent_dates = 
-        self.registered_on = 
-        self.expiry_date = 
-        self.last_updated = 
-        self.registration_status = 
-        self.nameservers = 
-        self.lookup_time = 
-        """
+        self.__url = WHOISHOST + url
+        p = MyParser(self.__url)
+        self.raw_output = ''
+        self.data = self.__clean_data(p.stack)
+        self.domain = self.__get_domain()
+        self.registrant = self.__get_registrant()
+        self.registrant_type = self.__get_registrant_type()
+        self.registrant_address = self.__get_registrant_address()
+        self.registrar = self.__get_registrar()
+        self.relevent_dates = self.__get_relevent_dates()
+        self.registered_on = self.__get_registered_on()
+        self.expiry_date = self.__get_expiry_date()
+        self.last_updated = self.__get_last_updated()
+        self.registration_status = self.__get_registration_status()
+        self.nameservers = self.__get_nameservers()
+        self.lookup_time = self.__get_lookup_time()
     
-    def f(self, x):
+    def __f(self, x):
         if x == '':
             pass
         if x == ' ':
@@ -47,31 +46,56 @@ class WhoisLookup(object):
         else:
             return x
 
-    def clean_data(self, data):
+    def __slice_boundary(self, value):
+        for k, v in enumerate(self.data):
+            if value in v:
+                return k
+
+    def __clean_data(self, data):
         data1 = ''.join(data)
         data2 = data1.split('\n')
         data3 = [line.replace('\r','') for line in data2]
-        data4 = filter(self.f, data3)
-        return data4
+        data4 = filter(self.__f, data3)
+        self.raw_output = data4
+        data = [line.strip() for line in data4]
+        return data
 
-    def get_domain(self, data):
-        iterdata = iter(data)
-        for line in iterdata:
-            if 'Domain name' in line:
-                return iterdata.next()
-        return "Domain not found"
+    def __get_domain(self):
+        return self.data[self.__slice_boundary('Domain name:') + 1 : self.__slice_boundary('Registrant:')]
 
-    def get_registrant(self, data):
-        iterdata = iter(data)
-        for line in iterdata:
-            if 'Registrant' in line:
-                return iterdata.next()
-        return "Registrant not found"        
+    def __get_registrant(self):
+        return self.data[self.__slice_boundary('Registrant:') + 1 : self.__slice_boundary('Registrant type:')]
+        
+    def __get_registrant_type(self):
+        return self.data[self.__slice_boundary('Registrant type:') + 1 : self.__slice_boundary("Registrant's address:")]
 
-    def get_registrant_type(self, data):
-        iterdata = iter(data)
-        for line in iterdata:
-            if 'Registrant type' in line:
-                return iterdata.next()
-        return "Registrant type not found"        
+    def __get_registrant_address(self):
+        return self.data[self.__slice_boundary("Registrant's address:") + 1 : self.__slice_boundary('Registrar:')]
 
+    def __get_registrar(self):
+        return self.data[self.__slice_boundary("Registrar") + 1 : self.__slice_boundary('Relevant dates:')]
+
+    def __get_relevent_dates(self):
+        return self.data[self.__slice_boundary("Relevant dates:") + 1 : self.__slice_boundary('Registration status:')]
+
+    def __get_registered_on(self):
+        r = self.data[self.__slice_boundary("Registered on:") : self.__slice_boundary("Registered on:") + 1]
+        return r[0].split(':')[1].strip()
+
+    def __get_expiry_date(self):
+        r = self.data[self.__slice_boundary("Expiry date:") : self.__slice_boundary("Expiry date:") + 1]
+        return r[0].split(':')[1].strip()
+
+    def __get_last_updated(self):
+        r = self.data[self.__slice_boundary("Last updated:") : self.__slice_boundary("Last updated:") + 1]
+        return r[0].split(':')[1].strip()
+
+    def __get_registration_status(self):
+        return self.data[self.__slice_boundary("Registration status:") + 1 : self.__slice_boundary('Name servers:')]
+
+    def __get_nameservers(self):
+        return self.data[self.__slice_boundary("Name servers:") + 1 : self.__slice_boundary('WHOIS lookup made at')]
+
+    def __get_lookup_time(self):
+        r = self.data[self.__slice_boundary("WHOIS lookup made at") : self.__slice_boundary("WHOIS lookup made at") + 1]
+        return r[0].split('at')[1].strip()
